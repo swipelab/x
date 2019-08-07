1e0ddcb5b1d029a5d98fde4876ece3333d723586
agrapine <alexandru@agrapine.com>
2019-08-07 15:36:10 +0100

    red pill

---
+++ ios/Classes/XPlugin.m
@@ -0,0 +1,20 @@
+#import "XPlugin.h"
+
+@implementation XPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"x"
+            binaryMessenger:[registrar messenger]];
+  XPlugin* instance = [[XPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
